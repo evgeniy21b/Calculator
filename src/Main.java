d29b93f29e8d39b5bb0274845a6b1f8d5f4306bd
evgeniy21b <48029851+evgeniy21b@users.noreply.github.com>
2023-02-27 23:50:43 +0300

    calculator1

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,34 @@
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args) throws Exception{
+public class Main {
+    public static void main(String[] args) throws Exception {
         Main.calc();
     }
+
     public static void calc() throws Exception {
-            Scanner scan = new Scanner(System.in);
-            String numbers = scan.nextLine();
-            String[] splitNumbers = numbers.split(" ");
-            if (splitNumbers.length == 0) {
-                throw new Exception("Неверное значение");
-            }
-            if (splitNumbers.length == 1) {
-                throw new Exception("Неверное значение");
-            }
+        Scanner scan = new Scanner(System.in);
+        String numbers = scan.nextLine();
+        String[] splitNumbers = numbers.split(" ");
+        if (splitNumbers.length == 0) {
+            throw new Exception("Неверное значение");
+        }
+        if (splitNumbers.length == 1) {
+            throw new Exception("Неверное значение");
+        }
 
 
-            var lenNumbers = numbers.length();
-            var num1 = splitNumbers[0];
-            var num2 = splitNumbers[2];
-            var operator = splitNumbers[1];
+
+        var num1 = splitNumbers[0];
+        var num2 = splitNumbers[2];
+        var operator = splitNumbers[1];
 
 
-            int x = Integer.parseInt(num1);
-            int y = Integer.parseInt(num2);
+        int x = Integer.parseInt(num1);
+        int y = Integer.parseInt(num2);
 
 
-            if ((lenNumbers >= 7) || (lenNumbers <= 3)) {
-                throw new Exception("Введенное значение неверно");
-            } else if ((x > 10) | (y > 10)) {
-                throw new Exception("Число не может быть больше 10");
-            } else if ((x < 1) | (y < 1)) {
-                throw new Exception("Число не может быть меньше 1");
-            } else if (operator.equals("+")) {
+        if (1 <= x && x <= 10 && 1 <= y && y <= 10) {
+            if (operator.equals("+")) {
                 System.out.println(x + y);
             } else if (operator.equals("-")) {
                 System.out.println(x - y);
@@ -44,5 +40,8 @@ public class Main{
                 throw new Exception("Неверное значение");
             }
 
+        } else {
+            throw new Exception("Неверное значение");
         }
     }
+}
